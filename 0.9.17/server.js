2d177064b1038fd2bd8b1be059094babd79e4b46
castor4bit <castor.4bit@gmail.com>
2014-07-23 06:21:13 +0900

    Add 0.9.17 test

---
+++ 0.9.17/server.js
@@ -0,0 +1,30 @@
+var argv = require('argv');
+var args = argv.option([
+  { name: 'port', short: 'p', type: 'int' }
+]).run();
+
+var port = args.options.port  || 4000;
+
+var io    = require('socket.io').listen(port);
+var debug = require('debug')('memoryleak-test');
+
+io.set("log level", 1);
+io.sockets.on('connection', function(socket) {
+  debug('connected: '+ socket.id);
+
+  socket.on('message', function(data) {
+    debug('receive message: '+ socket.id);
+
+    io.sockets.emit('message', data);
+  });
+  socket.on('disconnect', function() {
+    debug('disconnected: '+ socket.id);
+  });
+});
+
+setInterval(function() {
+  if (global.gc) {
+    global.gc();
+  }
+  console.log(process.memoryUsage());
+}, 5000);
